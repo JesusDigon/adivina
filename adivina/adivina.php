53181789cded2374355babfaa9c8fbe4296f7323
JesusDigon <46744751+JesusDigon@users.noreply.github.com>
2019-11-04 10:13:13 +0100

    Add files via upload

--- adivina/adivina.php
+++ adivina/adivina.php
@@ -6,30 +6,42 @@
 <body>
 <div id="contenido">
 	<?php
-	$numeroS=rand(1,20);
+	
 	session_start();
 	
-	echo $_SESSION['NumeroSecreto'];
 	print_r($_REQUEST);
-	if(!isset($_REQUEST['orden'])){
-		$_SESSION['NumeroSecreto']=$numeroS;
+	if(!isset($_SESSION['NumeroSecreto'])){
+	    $_SESSION['NumeroSecreto']=rand(1,20);
 		$_SESSION['intentos']=5;
-		include_once "formulario_adivina.html";
-		echo "<p>Bienvenido, Intente adivinar en qué número estoy pensando (1-20), tiene 5 intentos: </p>";
+		echo "<p>Bienvenido, Intente adivinar en qué número estoy pensando (1-20): </p>";
+		include_once "formulario_adivina.php";
+		
 		
 	}else{
 		switch ($_REQUEST['orden']) {
 			case "jugar":
 				if(is_numeric($_REQUEST['numero'])){
 					if(($_REQUEST['numero']) != $_SESSION['NumeroSecreto']){
+					  
+			
 						if($_REQUEST['numero']>$_SESSION['NumeroSecreto']){
 							echo"El número es más pequeño.";
 							$_SESSION['intentos']--;
+							if($_SESSION['intentos']==0){
+							    echo "<h1>Lo siento, has predido.<h1>";
+							    include_once 'seguir.html';
+							    exit();
+							}
 							include_once "formulario_adivina.php";
 							
 						}else{
 							echo"El número es más grande.";
 							$_SESSION['intentos']--;
+							if($_SESSION['intentos']==0){
+							    echo "<h1>Lo siento, has predido.<h1>";
+							    include_once 'seguir.html';
+							    exit();
+							}
 							include_once "formulario_adivina.php";
 						}
 					}else{
@@ -38,8 +50,14 @@
 					}
 				}else{
 					echo"Debe introducir un numero";
+					$_SESSION['intentos']--;
+					if($_SESSION['intentos']==0){
+					    echo "<h1>Lo siento, has predido.<h1>";
+					    include_once 'seguir.html';
+					    exit();
+					}
+					
 					include_once "formulario_adivina.php";
-
 				}
 			break;
 	
@@ -49,7 +67,7 @@
 				break;
 			case "si":
 				session_destroy();
-				include_once "formulario_adivina.php";
+				header("Refresh:0");
 				break;
 			case "no":
 				session_destroy();
@@ -62,4 +80,3 @@
 </div>
 </body>
 </html>
-
